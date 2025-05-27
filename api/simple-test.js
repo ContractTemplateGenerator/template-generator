c2d9d70479c65a81423f2f75055445fb0a215750
traffic7777 <sergei.tokmakov@gmail.com>
2025-05-27 04:48:20 -0700

    4

--- api/simple-test.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({ 
-    message: 'Simple test API works!',
-    method: req.method,
-    timestamp: new Date().toISOString()
-  });
-}
